228181f3fc2ad834501a4329b2a6c8ab44e2157a
masterng <nasterng@yansex.ru>
2022-03-22 23:59:23 +0500

    поправил слайдер

--- js/main.js
+++ js/main.js
@@ -3,7 +3,38 @@ $(document).ready(function() {
         infinite: true,
         slidesToShow: 6,
         slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+        responsive: [{
+                breakpoint: 1025,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 481,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 361,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 });
