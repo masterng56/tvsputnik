540daeb75b08871bf3e5c691f3e7396ab57f235f
masterng <nasterng@yansex.ru>
2022-03-22 23:40:47 +0500

    Блок акции готов

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.devices_slider').slick({
+        infinite: true,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+});
