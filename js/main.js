510193fee9a8e190c6b7672116091c9529b9cca4
masterng <nasterng@yansex.ru>
2022-03-24 12:38:39 +0500

    добавил стрелки в слайдер

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         infinite: true,
         slidesToShow: 6,
         slidesToScroll: 1,
-        autoplay: true,
+        // autoplay: true,
         autoplaySpeed: 2000,
         responsive: [{
                 breakpoint: 1440,
