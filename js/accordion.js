a3709fc5221264b0b53ee9ca51e736fbbb7aa38c
masterng <nasterng@yansex.ru>
2022-03-28 22:03:32 +0500

    сделал блок faq без анимации

---
+++ js/accordion.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.accordion_title').forEach((item) => item.addEventListener('click', () => {
+    const parent = item.parentNode;
+
+    if (parent.classList.contains('accordion_item_active')) {
+        parent.classList.remove('accordion_item_active');
+    } else {
+        document.querySelectorAll('.accordion_item').forEach((child) => child.classList.remove('accordion_item_active'))
+
+        parent.classList.toggle('accordion_item_active');
+    }
+}));
